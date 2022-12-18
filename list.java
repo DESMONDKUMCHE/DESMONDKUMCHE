b491dcf57c9e1e31371af2dbd2f29c5cb4870a2b
Desmond <desmondkumche@yahoo.com>
2022-12-18 07:47:39 +0000

    this i sthe second commit

---
+++ list.java
@@ -0,0 +1,17 @@
+#rerequisite
++ AWS Acccount.
++ Create Redhat EC2 T2.micro Instance.
++ Create Security Group and open Tomcat ports or Required ports.
+   + 8080 ..etc
++ Attach Security Group to EC2 Instance.
++ Install java openJDK 1.8+
+
+### Install  **<span style="color:green">Landmark Technologies, Ontario, Canada.</span>**
+### **<span style="color:green">Contacts: +1437 215 2483<br> WebSite : <http://mylandmarktech.com/></span>**
+### **Email: mylandmarktech@gmail.com**
+
+
+
+## Apache Tomcat Installation And Setup In AWS EC2 Redhat Instance.
+##### P Java JDK 1.8+ 
+
